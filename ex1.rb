94efdeafe15d056c3ffd9cc795ae34d71938dada
CampusCrow <campuscrow@gmail.com>
2015-08-25 19:36:18 -0700

    First Commit

---
+++ ex1.rb
@@ -0,0 +1,8 @@
+puts "Hello World!"
+puts "Hello Again"
+puts "I like typing this."
+# puts "this is fun."
+puts "Yay! Printing."
+puts "I'd much rather you 'not'.
+mess with this"
+puts "touch this",'I "said" do not touch this.'
